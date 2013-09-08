5b76eb738de0189baf85d395caec0b82d51f4b47
the.cat.2k@gmail.com <the.cat.2k@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-09-08 21:14:38 +0000

    Toptoo template

--- trunk/toptoo-2013/components/travelshop/ibe.offer_filter/.default/template.php
+++ trunk/toptoo-2013/components/travelshop/ibe.offer_filter/.default/template.php
@@ -1,9 +1,10 @@
-<? if (!defined('B_PROLOG_INCLUDED') || true !== B_PROLOG_INCLUDED) {
-  die();
-}
+<? if (!defined('B_PROLOG_INCLUDED') || true !== B_PROLOG_INCLUDED) { die(); } 
+
+$arParams['PENALTY_TYPE_FILTER_ENABLED'] = 'N';
 ?>
+
 <? if ( CIBEAjax::StartArea( "#ts_ag_all_in_one_offer_filter_container" ) ) { ?>
-<? if ( CIBEBufferedComponent::IsOutputEnabled() && !defined('__JS_IBE_OFFER_FILTER_TEMPLATE_DEFAULT') ): ?>
+  <? if ( CIBEBufferedComponent::IsOutputEnabled() && !defined('__JS_IBE_OFFER_FILTER_TEMPLATE_DEFAULT') ): ?>
   <? define('__JS_IBE_OFFER_FILTER_TEMPLATE_DEFAULT', true); ?>
   <? if (file_exists($_SERVER['DOCUMENT_ROOT'] . ($sStyleFile = $this->GetFolder() . '/styles.css'))): ?>
   <?= CIBECacheControl::RenderCSSLink( $sStyleFile ) ?>
@@ -11,33 +12,38 @@
   <? if (file_exists($_SERVER['DOCUMENT_ROOT'] . ($sScriptFile = $this->GetFolder() . '/js/script.js'))): ?>
   <?= CIBECacheControl::RenderJSLink( $sScriptFile ) ?>
   <? endif; ?>
-<? endif; // !defined('__JS_IBE_OFFER_FILTER_TEMPLATE_DEFAULT') ?>
+  <? endif; // !defined('__JS_IBE_OFFER_FILTER_TEMPLATE_DEFAULT') ?>
+
 <span id="ts_ag_offer_filter_container">
 <? if ( CIBEAjax::StartArea( "#ts_ag_offer_filter_container" ) ) { ?>
-<? $iEnabledFiltersCount = 0; ?>
-<? if ($arResult['FILTER']): ?>
-<div class="ts_ag_offer_filter"><div id="ts_ag_offer_filter<?= $arResult[ "~UID" ] ?>">
-  <h2><?= GetMessage('TS_IBE_OFFER_FILTER_TITLE') ?></h2>
-<? foreach ($arResult['FILTER'] as $arFilter): ?>
-<? if ($arFilter['~VISIBLE']): ?>
-  <? $enabler_name = $arFilter['NAME']."_FILTER_ENABLED"; ?>
-  <? if ( !isset( $arParams[ $enabler_name ] ) || $arParams[ $enabler_name ] !== "N" ): ?>
-  <? if ($arFilter['~ENABLED']) {
-    $iEnabledFiltersCount++;
-  } ?>
-  <h3 class="title<?= ($arFilter['~ENABLED'] ? ' enabled' : '') ?>">
-    <input type="hidden" class="body-id" value="<?= $arFilter['CLASSNAME'] ?><?= $arResult[ "~UID" ] ?>-block" />
-    <?= GetMessage($arFilter['TITLE']) ?> <span class="arr"></span>
-  </h3>
-  <? require dirname( __FILE__ ) . '/filter_body.php' ?>
-<? endif; // !isset( $arParams[ $enabler_name ] ) || $arParams[ $enabler_name ] !== "N" ?>
-<? endif; // $arFilter['~VISIBLE'] ?>
-<? endforeach; ?>
-<div id="disable-all-filters"><a href="javascript:void(0)" class="disable-all-filters"><?= GetMessage('TS_IBE_OFFER_FILTER_DISABLE_ALL_FILTERS') ?></a></div>
-</div></div>
+  <? $iEnabledFiltersCount = 0; ?>
+  <? if ($arResult['FILTER']): ?>
+  <div id="ts_ag_offer_filter">
+    <div id="ts_ag_offer_filter<?= $arResult[ "~UID" ] ?>">
+    <? foreach ($arResult['FILTER'] as $arFilter): ?>
+      <? if ($arFilter['~VISIBLE']): ?>
+        <? $enabler_name = $arFilter['NAME']."_FILTER_ENABLED"; ?>
+        <? if ( !isset( $arParams[ $enabler_name ] ) || $arParams[ $enabler_name ] !== "N" ): ?>
+          <? if ($arFilter['~ENABLED']) { $iEnabledFiltersCount++; } ?>
+      <h3 class="title<?= ($arFilter['~ENABLED'] ? ' enabled' : '') ?>">
+        <input type="hidden" class="body-id" value="<?= $arFilter['CLASSNAME'] ?><?= $arResult[ "~UID" ] ?>-block" />
+        <?= GetMessage($arFilter['TITLE']) ?>
+        <span class="arr"></span>
+      </h3>
+    
+      <? require dirname( __FILE__ ) . '/filter_body.php' ?>
+
+        <? endif; // !isset( $arParams[ $enabler_name ] ) || $arParams[ $enabler_name ] !== "N" ?>
+      <? endif; // $arFilter['~VISIBLE'] ?>
+    <? endforeach; ?>
+
+    <? /* <div id="disable-all-filters">
+      <a href="javascript:void(0)" class="disable-all-filters"><?= GetMessage('TS_IBE_OFFER_FILTER_DISABLE_ALL_FILTERS') ?></a>
+    </div> */ ?>
+  </div>
+</div>
 <script type="text/javascript">
 // <![CDATA[
-
 function getFilterIdsByFilterBody( filterBody ) {
   var arFilterIds = [];
 <? foreach ($arResult['FILTER'] as $arFilter): ?>
@@ -60,10 +66,7 @@ function getFilterIdsByFilterBody( filterBody ) {
 <? /* moved to js/script.js */ ?>
 new CIBEOfferFilterScript( '#ts_ag_offer_filter<?= $arResult[ "~UID" ] ?>', <?= $iEnabledFiltersCount ?>, '<?= $arResult[ "~UID" ] ?>' );
 
-if ('function' == typeof autoSelect) {
-  autoSelect();
-}
-
+if ('function' == typeof autoSelect) { autoSelect(); }
 // ]]>
 </script>
 <? endif; // if ($arResult['FILTER']) ?>
