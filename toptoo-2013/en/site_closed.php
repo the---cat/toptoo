5c354fe200302b4bd7b02cffa0192375ac0b311f
the.cat.2k@gmail.com <the.cat.2k@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-09-11 19:28:51 +0000

    Toptoo template

--- toptoo-2013/en/site_closed.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS ['SITE_CLOSED_TITLE'] = "The site is temporarily unavailable";
-$MESS ['SITE_CLOSED_DESC'] = "Sorry, this website is being upgraded at the moment.<br /><br />Please come back later.";
-?>
