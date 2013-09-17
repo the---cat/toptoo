59183856fcf08172b24ec7ff28b133bc5f45688e
klisin@gmail.com <klisin@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-09-17 09:26:28 +0000

    SEO-оптимизированный TITLE

---
+++ trunk/toptoo-2013/components/travelshop/ibe.frontoffice_all_in_one/lang/ru/component.php
@@ -0,0 +1,11 @@
+<?
+
+/* Заголовки экранов */
+$MESS[ "IBE_TITLE_OFFER" ] = "Поиск рейсов1" . ( $arLang["SITE_NAME"] ? " | ".$arLang["SITE_NAME"] : "" );
+$MESS[ "IBE_TITLE_PERSONAL_DATA" ] = "Пассажиры" . ( $arLang["SITE_NAME"] ? " | ".$arLang["SITE_NAME"] : "" );
+$MESS[ "IBE_TITLE_PRECOMMIT" ] = "Оплата и подтверждение" . ( $arLang["SITE_NAME"] ? " | ".$arLang["SITE_NAME"] : "" );
+$MESS[ "IBE_TITLE_PROGRESS" ] = "Обработка заказа" . ( $arLang["SITE_NAME"] ? " | ".$arLang["SITE_NAME"] : "" );
+$MESS[ "IBE_TITLE_ITINERARY" ] = "Маршрут-квитанция" . ( $arLang["SITE_NAME"] ? " | ".$arLang["SITE_NAME"] : "" );
+$MESS[ "IBE_TITLE_FINISH" ] = "Заказ готов" . ( $arLang["SITE_NAME"] ? " | ".$arLang["SITE_NAME"] : "" );
+
+?>
