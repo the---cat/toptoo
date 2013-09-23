b75cce36c9699caf7ab1aa7f8351a450eb360221
the.cat.2k@gmail.com <the.cat.2k@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-09-23 17:48:20 +0000

    Лучшие предложения

---
+++ trunk/toptoo-2013/components/travelshop/ibe.best_offers/.default/lang/ru/template.php
@@ -0,0 +1,23 @@
+<?
+$MESS[ "BEST_OFFERS_HEADER" ] = "Лучшие предложения";
+$MESS[ "BEST_OFFERS_DIRECTION_TO" ] = "Туда,";
+$MESS[ "BEST_OFFERS_DIRECTION_BACK" ] = "Обратно,";
+$MESS[ "BEST_OFFERS_CONNECTION" ] = "Пересадки:";
+$MESS[ "BEST_OFFERS_NO_CONNECTION" ] = "Без пересадок,";
+$MESS[ "BEST_OFFERS_FLIGHT_DURATION" ] = "в пути";
+$MESS[ "BEST_OFFERS_HOUR" ] = "ч";
+$MESS[ "BEST_OFFERS_MINUTE" ] = "мин";
+$MESS[ "BEST_OFFERS_SEATS" ] = "мест ";
+$MESS[ "BEST_OFFERS_SEATS_A_LOT" ] = "мест 4+";
+$MESS[ "BEST_OFFERS_FROM" ] = "из";
+
+$MESS[ "BEST_OFFERS_SELECT" ] = "Купить за";
+
+$MESS[ "BEST_OFFERS_TIMELIMIT_TITLE" ] = "Оплатить до";
+$MESS[ "BEST_OFFERS_SELECT_FLIGHTS" ] = "Другие рейсы по этой цене";
+
+$MESS[ "BEST_OFFERS_PROFILE_LOW_COST" ] = "Выгодная цена";
+$MESS[ "BEST_OFFERS_PROFILE_FIT" ] = "Удобный";
+$MESS[ "BEST_OFFERS_PROFILE_FASTEST" ] = "Быстрый перелет";
+$MESS[ "BEST_OFFERS_PROFILE_TOP_SALE" ] = "Хит продаж";
+?>
