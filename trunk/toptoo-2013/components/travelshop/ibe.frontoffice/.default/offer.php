84ebc0c1cfe4e63150448393c52f8edfb32bde5d
the.cat.2k@gmail.com <the.cat.2k@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-09-26 15:19:02 +0000

    Съезжает пересадка

--- trunk/toptoo-2013/components/travelshop/ibe.frontoffice/.default/offer.php
+++ trunk/toptoo-2013/components/travelshop/ibe.frontoffice/.default/offer.php
@@ -87,92 +87,102 @@ echo $arResult['SCRIPT']; ?>
           </tr>
         </thead>
         <? foreach($flights['FLIGHT'] as $flightKey => $flight):
-        $rowspan = (($segments = count($flight['SEGMENTS'])) > 1 ? ' rowspan="'.$segments.'"' : ""); ?>
-        <tbody class="variant dir_<?= (0 == $directionKey ? 'to' : 'back') ?><? if($flight['INPUT']['~SELECTED']){ ?> selected<? } ?>" id="var-<?= $offerKey ?>-<?= $directionKey ?>-<?= $flightKey ?>">
+        $rowspan = (($segments = count($flight['SEGMENTS'])) > 1 ? ' rowspan="'.($segments + ($segments-1)*2).'"' : ""); ?>
+        <tbody class="variant dir_<?= (0 == $directionKey ? 'to' : 'back') ?><? if($flight['INPUT']['~SELECTED']){ ?> selected<? } ?><?= $segments > 1 ? ' with_stops' : '' ?>" id="var-<?= $offerKey ?>-<?= $directionKey ?>-<?= $flightKey ?>">
           <? for($seg_ct=0; $seg_ct<$segments; $seg_ct++): ?>
-          <tr class="flight<?= $segments > 1 ? ' with_stops' . ($seg_ct==0 ? ' first' : '' ) . ( $seg_ct == $segments-1 ? ' last' : '' ) : '' ?>">
+
+          <? if ( is_array($flight['SEGMENTS'][$seg_ct]['STOP_DURATION']) ) { ?>
+            <? //trace ($flight['SEGMENTS'][$seg_ct]);
+            list($ah, $am) = explode ( ':', $flight['SEGMENTS'][$seg_ct-1]['ARRIVAL']['TIME'] );
+            list($dh, $dm) = explode ( ':', $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['TIME'] );
+            $arrTime = intval($ah)*60 + intval($am);
+            $depTime = intval($dh)*60 + intval($dh);
+            $night = ( 
+              ( $arrTime > 23*60 ) ||
+              ( $arrTime < 5*60 ) ||
+              ( $depTime < 7*60 ) ||
+              ( $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['DATE'] != $flight['SEGMENTS'][$seg_ct-1]['ARRIVAL']['DATE'] )
+            ) ? true : false;
+            $long = intval($flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~HOURS']) > 3 ? true : false; ?>
+          <tr class="stop_wrap">
+            <td>&nbsp;</td>
+            <td colspan="2"> 
+              <div>
+                <div class="stop<?= ( $night ? ' night' : '' ) . ( $long ? ' long' : '' ) ?>">
+                  <?= $long ? GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER_LONG') : '' ?>
+                  <?= $night ? GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER_NIGHT') : '' ?>
+                  <?= GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER') ?>
+                  <?= 
+                  ( $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~HOURS'] ? $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~HOURS'] . '&nbsp;' . GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER_H') : '' ) .
+                  ( $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~HOURS'] && $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~MINUTES'] ? '&nbsp;' : '' ) .
+                  ( $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~MINUTES'] ? $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~MINUTES'] . '&nbsp;' . GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER_MIN') : '')
+                  ?>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr class="stop_wrap_add"><td>&nbsp;</td><td colspan="2">&nbsp;</td></tr>
+          <? } ?>
+
+          <tr class="flight<?= ($seg_ct==0 ? ' first' : '' ) . ( $seg_ct == $segments-1 ? ' last' : '' ) ?>">
             <? if($seg_ct == 0): ?>
             <td class="select"<?=$rowspan ?>>
               <input id="input-<?=$offer['~ID'].$flight['INPUT']['~ID']?>" type="<?=$flight['INPUT']['~TYPE'] ?>" name="<?=$flight['INPUT']['NAME'] ?>" value="<?=$flight['INPUT']['VALUE']?>"<? if($flight['INPUT']['~SELECTED']): ?> checked="checked"<? endif; ?><? if ( !$bValidOffer ) : ?> disabled="disabled"<? endif; ?> />
             </td>
             <? endif; ?>
-            <? $akLogo = $arResult['LOGOS'][$flight['SEGMENTS'][$seg_ct]['~CODE']] ?>
-            <td<?= ($arResult['LOGOS'] ? ' class="logo logo-normal-' . $akLogo['IATACODE'] . '"' : '') ?>>
-              <div class="number">
+            <td class="flight_number">
               <? if ($arResult['LOGOS']): ?>
-                <span class="ak-name" title="<?= $akLogo['TITLE']; ?>"><?= $akLogo['TITLE']; ?></span>
-              <? endif; ?>
+              <? $akLogo = $arResult['LOGOS'][$flight['SEGMENTS'][$seg_ct]['~CODE']] ?>
+              <div class="logo<?= ($arResult['LOGOS'] ? ' logo-normal-' . $akLogo['IATACODE'] . '"' : '') ?>" title="<?= $akLogo['TITLE']; ?>"></div><? 
+              endif; 
+              ?><div class="number_plane">
+                <div class="number">
               <?=$flight['SEGMENTS'][$seg_ct]['~AK'] ?>&nbsp;<?=$flight['SEGMENTS'][$seg_ct]['NUMBER'] ?><? 
               if( !empty( $flight['SEGMENTS'][$seg_ct]['~OAK'] ) && $flight['SEGMENTS'][$seg_ct]['~OAK'] != $flight['SEGMENTS'][$seg_ct]['~AK'] ): 
                 ?><sup class="oak" title="<?=GetMessage("TS_FRONTOFFICE_STEP2_OFFER_OAK_TITLE") ?> <?=$flight['SEGMENTS'][$seg_ct]['OAK'] ?>">*</sup>
               <? endif; ?>
                 </div>
                 <div class="plane"><?=$flight['SEGMENTS'][$seg_ct]['PLANE_NAME'] ?></div>
-              </td>
-              <td class="flight_info">
-                <? if ( is_array($flight['SEGMENTS'][$seg_ct]['STOP_DURATION']) ) { ?>
-                    <? //trace ($flight['SEGMENTS'][$seg_ct]);
-                    list($ah, $am) = explode ( ':', $flight['SEGMENTS'][$seg_ct-1]['ARRIVAL']['TIME'] );
-                    list($dh, $dm) = explode ( ':', $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['TIME'] );
-                    $arrTime = intval($ah)*60 + intval($am);
-                    $depTime = intval($dh)*60 + intval($dh);
-                    $night = ( 
-                      ( $arrTime > 23*60 ) ||
-                      ( $arrTime < 5*60 ) ||
-                      ( $depTime < 7*60 ) ||
-                      ( $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['DATE'] != $flight['SEGMENTS'][$seg_ct-1]['ARRIVAL']['DATE'] )
-                    ) ? true : false;
-                    $long = intval($flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~HOURS']) > 3 ? true : false; ?>
-                <div class="stop_wrap">
-                  <div class="stop<?= ( $night ? ' night' : '' ) . ( $long ? ' long' : '' ) ?>">
-                    <?= $long ? GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER_LONG') : '' ?>
-                    <?= $night ? GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER_NIGHT') : '' ?>
-                    <?= GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER') ?>
-                    <?= 
-                    ( $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~HOURS'] ? $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~HOURS'] . '&nbsp;' . GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER_H') : '' ) .
-                    ( $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~HOURS'] && $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~MINUTES'] ? '&nbsp;' : '' ) .
-                    ( $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~MINUTES'] ? $flight['SEGMENTS'][$seg_ct]['STOP_DURATION']['~MINUTES'] . '&nbsp;' . GetMessage('TS_FRONTOFFICE_STEP2_OFFER_STOPOVER_MIN') : '')
-                    ?>
-                  </div>
-                </div>
-                  <? } ?>
-                <div class="departure">
-                  <span class="time"><?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['TIME'] ?></span>
-                  <span class="date"><?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['DATE'] ?></span>
-                  <div class="point"><?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['LOC_NAME'] ?></div>
-                  <div class="airport"><?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['APT_NAME'] ? $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['APT_NAME'] : $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['LOC_NAME'] ?> <span class="code">/ <?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['~APT_CODE'] ?></span></div>
-                </div>
-                <div class="arrival">
-                  <span class="time"><?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['TIME'] ?></span>
-                  <span class="date"><?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['DATE'] ?></span>
-                  <div class="point"><?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['LOC_NAME'] ?></div>
-                  <div class="airport"><?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['APT_NAME'] ? $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['APT_NAME'] : $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['LOC_NAME'] ?> <span class="code">/ <?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['~APT_CODE'] ?></span></div>
-                </div>
-              </td>
-              <td class="add_info">
+              </div>
+            </td>
+            <td class="flight_info">
+              <div class="departure">
+                <span class="time"><?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['TIME'] ?></span>
+                <span class="date"><?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['DATE'] ?></span>
+                <div class="point"><?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['LOC_NAME'] ?></div>
+                <div class="airport"><?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['APT_NAME'] ? $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['APT_NAME'] : $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['LOC_NAME'] ?> <span class="code">/ <?= $flight['SEGMENTS'][$seg_ct]['DEPARTURE']['~APT_CODE'] ?></span></div>
+              </div>
+              <div class="arrival">
+                <span class="time"><?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['TIME'] ?></span>
+                <span class="date"><?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['DATE'] ?></span>
+                <div class="point"><?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['LOC_NAME'] ?></div>
+                <div class="airport"><?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['APT_NAME'] ? $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['APT_NAME'] : $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['LOC_NAME'] ?> <span class="code">/ <?= $flight['SEGMENTS'][$seg_ct]['ARRIVAL']['~APT_CODE'] ?></span></div>
+              </div>
+            </td>
+            <td class="add_info">
               <? if ( $segments < 2 && empty($flight['~STOPS']) ) : ?>
-                <div class="no_stops"><?= GetMessage('TS_FRONTOFFICE_STEP2_OFFER_NO_STOPS') ?></div>
+              <div class="no_stops"><?= GetMessage('TS_FRONTOFFICE_STEP2_OFFER_NO_STOPS') ?></div>
               <? endif; ?>
 
               <? if( $arResult[ 'SHOW_SEAT_WARNING' ] ): ?>
-                <div class="seats<? if ( $flight['ALL_SEATS']['~COUNT'] < 4 ){ ?> last_seats<? } ?>"<?=$rowspan ?>>
+              <div class="seats<? if ( $flight['ALL_SEATS']['~COUNT'] < 4 ){ ?> last_seats<? } ?>"<?=$rowspan ?>>
                 <? if ( $flight['ALL_SEATS']['~COUNT'] > 3 ): ?><?= GetMessage('TS_FRONTOFFICE_STEP2_OFFER_SEATS_INFO') ?>
                 <? else: ?><?= $flight[ 'ALL_SEATS' ][ 'INFO' ] ?><? endif; ?>
-                </div>
+              </div>
               <? endif; ?>
 
-                <div class="service_class">
-                  <?=$flight['SEGMENTS'][$seg_ct]['SERVICE_CLASS'] ?> <span class="service_code">(<?=$flight['SEGMENTS'][$seg_ct]['~SERVICE_CODE'] ?>)</span>
-                </div>
+              <div class="service_class">
+                <?=$flight['SEGMENTS'][$seg_ct]['SERVICE_CLASS'] ?> <span class="service_code">(<?=$flight['SEGMENTS'][$seg_ct]['~SERVICE_CODE'] ?>)</span>
+              </div>
 
-                <div class="flight_duration">
+              <div class="flight_duration">
                 <? if( $segments > 1 ){ $duration = $flight['SEGMENTS'][$seg_ct]['DURATION']; }
                 else { $duration = $flight['~TIME']; }
                 list ($hh, $mm) = explode( ':', $duration ) ?>
                 <?= GetMessage('TS_FRONTOFFICE_STEP2_OFFER_FLIGHT_DURATION') ?>
                 <?= ( floor($hh) ? floor($hh) . '&nbsp;' . GetMessage('IBE_FRONTOFFICE_H') : '' ) . ( floor($hh) && floor($mm) ? '&nbsp;' : '' ) . ( floor($mm) ? floor($mm) . '&nbsp;' . GetMessage('IBE_FRONTOFFICE_MIN') : '' )?>
-                </div>
-              </td>
+              </div>
+            </td>
           </tr>
           <? endfor; ?>
         </tbody>
