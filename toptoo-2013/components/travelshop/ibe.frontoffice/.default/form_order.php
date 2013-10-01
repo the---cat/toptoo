97413a9d9ea782493ddc085e4de3e9dd9e05ea52
klisin@gmail.com <klisin@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-10-01 18:10:04 +0000

    Issue 17. Поисковая форма. Перевод фокуса.

--- toptoo-2013/components/travelshop/ibe.frontoffice/.default/form_order.php
+++ toptoo-2013/components/travelshop/ibe.frontoffice/.default/form_order.php
@@ -252,10 +252,11 @@ function calendarsSetup() {
     onSelect: function(dateText) {
       selectForwardDate(dateText);
       if ( "RT" == $("#ts_ag_reservation #RT_OW").val() ) {
-        $("#ts_ag_reservation #dateback_formated").click();
-        if ( e.stopPropagation ) {
-          e.stopPropagation();
-        }
+        $("#ts_ag_reservation #dateback_formated").click(function(e){
+          if ( e.stopPropagation ) {
+            e.stopPropagation();
+          }
+        });
       } else {
         $("#ts_ag_reservation #form_order_submit").focus();
       };
