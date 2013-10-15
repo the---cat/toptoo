89c4d62d964caa157add78312e7a24dd83a6b085
the.cat.2k@gmail.com <the.cat.2k@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-10-15 11:43:44 +0000

    Issue 32:	Подготовить фрейм для установки на стороне www.new.toptoo.com

--- toptoo-frame-2013/footer.php
+++ toptoo-frame-2013/footer.php
@@ -1,5 +1,18 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <? if ( defined("SHOW_404") || SHOW_404 == "Y") { return; } ?>
+<div class="sect_bottom">
+<? $APPLICATION->IncludeComponent(
+  "bitrix:main.include",
+  "",
+  Array(
+    "AREA_FILE_SHOW" => "sect", 
+    "AREA_FILE_SUFFIX" => "bottom", 
+    "EDIT_MODE" => "html", 
+    "EDIT_TEMPLATE" => "standart.php",
+    "AREA_FILE_RECURSIVE" => "N" 
+  )
+); ?>
+</div>
 <script type="text/javascript">
 // <![CDATA[
 tooltip();
