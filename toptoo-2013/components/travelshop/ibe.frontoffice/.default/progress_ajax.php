235bfaaebadcdcf7229789ba8c1e39292a5361ed
the.cat.2k@gmail.com <the.cat.2k@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-10-16 15:42:10 +0000

    Issue 33:	Спиннер для авиации

--- toptoo-2013/components/travelshop/ibe.frontoffice/.default/progress_ajax.php
+++ toptoo-2013/components/travelshop/ibe.frontoffice/.default/progress_ajax.php
@@ -4,5 +4,16 @@
   <div class="progress-text" id="progress-text">
     <?= GetMessage( 'IBE_WWINDOW_PLEASE_WAIT' ) ?>
   </div>
-  <img class="progress-img" src="<?= $templateFolder."/images/wait.png" ?>" alt="" />
+  <? /*<img class="progress-img" src="<?= $templateFolder."/images/wait.png" ?>" alt="" /> */?>
+  <? $APPLICATION->IncludeComponent(
+  "bitrix:main.include",
+  "",
+  Array(
+    "AREA_FILE_SHOW" => "sect", 
+    "AREA_FILE_SUFFIX" => "spinner", 
+    "EDIT_MODE" => "html", 
+    "EDIT_TEMPLATE" => "standart.php",
+    "AREA_FILE_RECURSIVE" => "N" 
+  )
+); ?>
 </div>
