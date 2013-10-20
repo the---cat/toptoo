1be16371cead4a96299a57f8bffadd4d65074487
the.cat.2k@gmail.com <the.cat.2k@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-10-20 21:06:20 +0000

    Issue 33:	Спиннер для авиации Issue 46:	Оформление ошибок Issue 47:	в хроме и сафари на результатах поиска фильтры закрыты

--- toptoo-2013/components/travelshop/ibe.frontoffice/.default/progress_ajax.php
+++ toptoo-2013/components/travelshop/ibe.frontoffice/.default/progress_ajax.php
@@ -1,9 +1,6 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <? //trace($arResult) ?>
 <div id="progress_ajax">
-  <div class="progress-text" id="progress-text">
-    <?= GetMessage( 'IBE_WWINDOW_PLEASE_WAIT' ) ?>
-  </div>
   <? /*<img class="progress-img" src="<?= $templateFolder."/images/wait.png" ?>" alt="" /> */?>
   <? $APPLICATION->IncludeComponent(
   "bitrix:main.include",
