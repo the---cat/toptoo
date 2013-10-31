40df0aafb86089ebfcfb8fa15ed055ffb04a44b0
klisin@gmail.com <klisin@gmail.com@ba6cd8e6-2238-22fe-59d9-54b327b1714d>
2013-10-31 14:28:10 +0000

    Issue 49: 	работа полей откуда / куда на планшете

--- trunk/toptoo-frame-2013/components/travelshop/ibe.shortform/.default/template.php
+++ trunk/toptoo-frame-2013/components/travelshop/ibe.shortform/.default/template.php
@@ -430,7 +430,7 @@ safeCall(calendarsSetup);
 <? endif; // if ( is_array($arResult["ROUTES"]) && count($arResult["ROUTES"]) ): ?>
 
 $( document ).ready( function() {
-  $('.input').bind('click', function(){
+  $('#ts_ag_quick_reservation_form .location input.text').bind('click', function(){
     $(this).focus();
   });
 });
